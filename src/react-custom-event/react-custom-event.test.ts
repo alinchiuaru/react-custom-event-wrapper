ab25456c3039ad031e58ebdc2d710ad2f9eade49
Alin Chiuaru <alin.chiuaru@gmail.com>
2021-08-27 15:47:59 +0300

    Update tests

--- src/react-custom-event/react-custom-event.test.ts
+++ src/react-custom-event/react-custom-event.test.ts
@@ -11,9 +11,15 @@ import { renderHook, cleanup } from "@testing-library/react-hooks";
 describe("Adding, removing and dispatching of custom events", () => {
   const testEvent = createCustomEvent<number>("TEST_EVENT");
   const eventPayload = 444;
+  let cbFn: (...args: any[]) => any;
+
+  beforeEach(() => {
+    cbFn = jest.fn((e: CustomEvent) => {
+      return e.detail;
+    });
+  });
 
   it("should dispatch a custom event", () => {
-    const cbFn = jest.fn();
     document.addEventListener(testEvent.type, cbFn);
 
     dispatchCustomEvent(testEvent(1));
@@ -23,20 +29,12 @@ describe("Adding, removing and dispatching of custom events", () => {
 
   describe("Register and remove custom event listener", () => {
     it("should callback with the correct detail data", () => {
-      const cbFn = jest.fn((e: CustomEvent) => {
-        return e.detail;
-      });
-
       addEventListener(testEvent, cbFn);
       dispatchCustomEvent(testEvent(eventPayload));
       expect(cbFn).toHaveReturnedWith(eventPayload);
     });
 
     it("should remove a custom event listener", () => {
-      const cbFn = jest.fn((e: CustomEvent) => {
-        return e.detail;
-      });
-
       addEventListener(testEvent, cbFn);
       dispatchCustomEvent(testEvent(eventPayload));
 
@@ -50,23 +48,36 @@ describe("Adding, removing and dispatching of custom events", () => {
 describe("useCustomEventListener hook tests", () => {
   const testEvent = createCustomEvent<number>("TEST_EVENT");
   const eventPayload = 444;
+  let cbFn: (...args: any[]) => any;
 
-  it("should callback with the correct detail data", () => {
-    const cbFn = jest.fn((e: CustomEvent) => {
+  beforeEach(() => {
+    cbFn = jest.fn((e: CustomEvent) => {
       return e.detail;
     });
 
     renderHook(() => useCustomEventListener(testEvent, cbFn));
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should callback with the correct detail data", () => {
     dispatchCustomEvent(testEvent(eventPayload));
     expect(cbFn).toHaveReturnedWith(eventPayload);
   });
 
-  it("should remove the event listener on cleanup", () => {
-    const cbFn = jest.fn((e: CustomEvent) => {
-      return e.detail;
-    });
+  it("should callback the correct amount of times", () => {
+    const times = 3;
 
-    renderHook(() => useCustomEventListener(testEvent, cbFn));
+    for (let i = 0; i < times; i++) {
+      dispatchCustomEvent(testEvent(eventPayload));
+    }
+
+    expect(cbFn).toHaveBeenCalledTimes(times);
+  });
+
+  it("should remove the event listener on cleanup", () => {
     dispatchCustomEvent(testEvent(eventPayload));
 
     cleanup();
